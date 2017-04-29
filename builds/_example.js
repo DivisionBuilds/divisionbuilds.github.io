82866c1f8c86a25b3efc993062989bdb69ff16a9
Zerthox <zerthox@gmail.com>
2017-04-29 23:04:31 +0200

    Update _example.js

--- builds/_example.js
+++ builds/_example.js
@@ -4,7 +4,7 @@
 	"link": "https://yourwebsitelink.com", // [OPTIONAL]
 	"date": "29/04/2017", // The date you last updated the build (content wise)
 	"patch": "1.6", // The patch this build is for [OPTIONAL]
-	"tags": ["tag1", "tag2", "tag3"], // Tags: pvp, pve, dps, healer, tank etc.
+	"tags": ["tag1", "tag2", "tag3"], // Tags: "pvp", "pve", "dps", "healer", "tank" etc.
 	"build": {
 		"weapons": {
 			"primary": {
@@ -29,62 +29,62 @@
 				"chest": {
 					"name": "Vigorous Chest", // Chest name/type
 					"text": "Health, Skill Haste, Burn Resistance", // Chest rolls
-					"type": "he", // Type: he, exotic or gearset
+					"type": "he", // Type: "he", "exotic" or "gearset"
 					"icon": "chest" // "chest" if he/exotic, else gearset name
 				},
 				"backpack": {
 					"name": "Specialized Backpack", // Backpack name/type
 					"text": "Health, Burn Resistance", // Backpack rolls
-					"type": "he", // Type: he, exotic or gearset
+					"type": "he", // Type: "he", "exotic" or "gearset"
 					"icon": "backpack" // "backpack" if he/exotic, else gearset name
 				},
 				"mask": {
 					"name": "Tenacious Mask", // Mask name/type
 					"text": "Health, Burn Resistance", // Mask rolls
-					"type": "he", // Type: he, exotic or gearset
+					"type": "he", // Type: "he", "exotic" or "gearset"
 					"icon": "mask" // "chemaskst" if he/exotic, else gearset name
 				},
 				"gloves": {
 					"name": "Skulls MC Gloves", // Gloves name/type
 					"text": "Assault Rifle Damage, SMG Damage, Critical Hit Chance", // Gloves rolls
-					"type": "exotic", // Type: he, exotic or gearset
+					"type": "exotic", // Type: "he", "exotic" or "gearset"
 					"icon": "gloves" // "gloves" if he/exotic, else gearset name
 				},
 				"holster": {
 					"name": "Nimble Holster", // Holster name/type
 					"text": "Health", // Holster rolls
-					"type": "he", // Type: he, exotic or gearset
+					"type": "he", // Type: "he", "exotic" or "gearset"
 					"icon": "holster" // "holster" if he/exotic, else gearset name
 				},
 				"knee-pads": {
 					"name": "Shortbow Championship Knee Pads", // Knee Pads name/type
 					"text": "Health, Burn Resistance, Shock Resistance, Disrupt Resistance", // Knee Pads rolls
-					"type": "exotic", // Type: he, exotic or gearset
+					"type": "exotic", // Type: "he", "exotic" or "gearset"
 					"icon": "knee-pads" // "knee-pads" if he/exotic, else gearset name
 				}
 			},
 			"stats": {
 				"main-stats": { // Main stat rolls on your gear pieces (Chest, Backpack, mask, Gloves, Knee pads => Total: 5)
-					"4x": {
-						"icon": "firearms",
-						"name": "Firearms Roll"
+					"4x": { // Amount of this type of roll
+						"name": "Firearms Roll", // Firearms, Stamina or Electronics roll?
+						"icon": "firearms" // icon of the main stat
 					},
-					"1x": {
-						"icon": "electronics",
-						"name": "Electronics Roll"
+					"1x": { // Amount of this type of roll
+						"name": "Electronics Roll", // Firearms, Stamina or Electronics roll?
+						"icon": "electronics" // icon of the main stat
 					}
 				},
 				"mods": { // Main stat mods on your gear (2x Chest, 1x Backpack, 1x Mask, 1x Knee Pads => Total: 5)
-					"5x": {
-						"icon": "firearms",
-						"name": "Firearms Mod",
-						"text": "Health"
+					"5x": { // Amount of this type of Mod
+						"name": "Firearms Mod", // Mod name
+						"text": "Health", // Mod roll
+						"icon": "firearms" // Icon of the mods main stat
 					}
 				},
 				"performance": { // Performance mods on your gear (2x Backpack, 1x Holster, 1x Knee Pads => Total: 4)
-					"4x": {
-						"icon": "first-aid",
-						"name": "First Aid Self Heal Mod"
+					"4x": { // Amount of this type of Performance Mod
+						"name": "First Aid Self Heal Mod", // Performance mod name
+						"icon": "first-aid" // Icon of the skill you modify
 					}
 				}
 			}
