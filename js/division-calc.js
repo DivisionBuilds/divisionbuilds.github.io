d46fefc64968b5e7bf9cde2de452345dc04352a7
Zerthox <zerthox@gmail.com>
2017-05-01 16:54:51 +0200

    Update division-calc.js

--- js/division-calc.js
+++ js/division-calc.js
@@ -37,9 +37,9 @@ var calc = {
 			return [min, max];
 		}
 	},
-	mitigation: functio (arm) {
+	mitigation: function(arm) {
 		return arm / 239;
-	}
+	},
 	toughness: function(health, mitigation) {
 		return health / ((100 - mitigation) / 100);
 	},
