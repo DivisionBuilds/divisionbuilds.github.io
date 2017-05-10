1f1d3ac35e92f12eba30af4e46df8ea9524fa0e4
Zerthox <zerthox@gmail.com>
2017-05-10 09:31:45 +0200

    Add weapons & mods

--- data/getdata.js
+++ data/getdata.js
@@ -3,6 +3,24 @@ var data = {};
 $.ajax({
 	url: "https://spreadsheets.google.com/feeds/list/1JTLivI7FDB0HeHIX0GePMLJd4bD-xarH3ZpNxPCX218/9/public/basic?alt=json",
 	success: function(result) {
-		data = JSON.parse(result);
+		data.gear = result;
+	}
+});
+$.ajax({
+	url: "https://spreadsheets.google.com/feeds/list/1JTLivI7FDB0HeHIX0GePMLJd4bD-xarH3ZpNxPCX218/10/public/basic?alt=json",
+	success: function(result) {
+		data.gearmods = result;
+	}
+});
+$.ajax({
+	url: "https://spreadsheets.google.com/feeds/list/1JTLivI7FDB0HeHIX0GePMLJd4bD-xarH3ZpNxPCX218/16/public/basic?alt=json",
+	success: function(result) {
+		data.weapons = result;
+	}
+});
+$.ajax({
+	url: "https://spreadsheets.google.com/feeds/list/1JTLivI7FDB0HeHIX0GePMLJd4bD-xarH3ZpNxPCX218/18/public/basic?alt=json",
+	success: function(result) {
+		data.weaponmods = result;
 	}
 });
