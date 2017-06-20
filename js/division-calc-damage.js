634ceb2bdba9c68de1ab057805fb7c538fdb0e21
Zerthox <zerthox@gmail.com>
2017-06-20 18:40:34 +0200

    Update damage calculator

--- js/division-calc-damage.js
+++ js/division-calc-damage.js
@@ -1,27 +1,41 @@
+    var page = {
+        change: function() {
+            var o = {};
+            $("input").each(function() {
+                o[$(this).attr("class")] = parseFloat($(this).val());
+            });
+            o.increase = [];
+            $(".increase").each(function() {
+                o.increase[o.increase.length] = parseFloat($(this).val());
+            });
+            $(".bulletDMG .value").text(Math.round(calc.bulletDMG({base: o.base, firearms: o.firearms, ratio: o.ratio, increase: o.increase})));
+            $(".avgbulletDMG .value").text(Math.round(calc.bulletDMG(o)));
+            $(".pve-bulletDMG .value").text(Math.round(calc.pve.bulletDMG(o)));
+            $(".pve-burstDMG .value").text(Math.round(calc.pve.burstDMG(o)));
+            $(".pve-burstDPS .value").text(Math.round(calc.pve.burstDPS(o)));
+            $(".pve-DPS .value").text(Math.round(calc.pve.DPS(o)));
+            $(".pvp-bulletDMG .value span:eq(0)").text(Math.round(calc.pvp.bulletDMG(o).min));
+            $(".pvp-bulletDMG .value span:eq(1)").text(Math.round(calc.pvp.bulletDMG(o).max));
+            $(".pvp-burstDMG .value span:eq(0)").text(Math.round(calc.pvp.burstDMG(o).min));
+            $(".pvp-burstDMG .value span:eq(1)").text(Math.round(calc.pvp.burstDMG(o).max));
+            $(".pvp-burstDPS .value span:eq(0)").text(Math.round(calc.pvp.burstDPS(o).min));
+            $(".pvp-burstDPS .value span:eq(1)").text(Math.round(calc.pvp.burstDPS(o).max));
+            $(".pvp-DPS .value span:eq(0)").text(Math.round(calc.pvp.DPS(o).min));
+            $(".pvp-DPS .value span:eq(1)").text(Math.round(calc.pvp.DPS(o).max));
+            $(".ls-bulletDMG .value").text(Math.round(calc.pvp.bulletDMG(o).ls));
+            $(".ls-burstDMG .value").text(Math.round(calc.pvp.burstDMG(o).ls));
+            $(".ls-burstDPS .value").text(Math.round(calc.pvp.burstDPS(o).ls));
+            $(".ls-DPS .value").text(Math.round(calc.pvp.DPS(o).ls));
+        }
+    };
 $(document).ready(function() {
 	$("head").append("<script src='/js/division-calc.js'></script>");
-    $("input").on("keyup change", function() {
-    	var o = {};
-    	$("input").each(function() {
-    		o[$(this).attr("class")] = parseFloat($(this).val());
-    	});
-    	$(".bulletDMG .value").text(Math.round(calc.bulletDMG({base: o.base, firearms: o.firearms, ratio: o.ratio, increase: o.increase})));
-    	$(".avgbulletDMG .value").text(Math.round(calc.bulletDMG(o)));
-    	$(".pve-bulletDMG .value").text(Math.round(calc.pve.bulletDMG(o)));
-    	$(".pve-burstDMG .value").text(Math.round(calc.pve.burstDMG(o)));
-    	$(".pve-burstDPS .value").text(Math.round(calc.pve.burstDPS(o)));
-    	$(".pve-DPS .value").text(Math.round(calc.pve.DPS(o)));
-    	$(".pvp-bulletDMG .value span:eq(0)").text(Math.round(calc.pvp.bulletDMG(o).min));
-    	$(".pvp-bulletDMG .value span:eq(1)").text(Math.round(calc.pvp.bulletDMG(o).max));
-    	$(".pvp-burstDMG .value span:eq(0)").text(Math.round(calc.pvp.burstDMG(o).min));
-    	$(".pvp-burstDMG .value span:eq(1)").text(Math.round(calc.pvp.burstDMG(o).max));
-    	$(".pvp-burstDPS .value span:eq(0)").text(Math.round(calc.pvp.burstDPS(o).min));
-    	$(".pvp-burstDPS .value span:eq(1)").text(Math.round(calc.pvp.burstDPS(o).max));
-    	$(".pvp-DPS .value span:eq(0)").text(Math.round(calc.pvp.DPS(o).min));
-    	$(".pvp-DPS .value span:eq(1)").text(Math.round(calc.pvp.DPS(o).max));
-        $(".ls-bulletDMG .value").text(Math.round(calc.pvp.bulletDMG(o).ls));
-        $(".ls-burstDMG .value").text(Math.round(calc.pvp.burstDMG(o).ls));
-        $(".ls-burstDPS .value").text(Math.round(calc.pvp.burstDPS(o).ls));
-        $(".ls-DPS .value").text(Math.round(calc.pvp.DPS(o).ls));
+    $("input").on("keyup change", page.change);
+    $(".button:contains('Add')").click(function() {
+        $(".append").append('<div class="flex-child added"><input type="number" value="0" min="0" class="increase"></div>');
+        $("input").on("keyup change", page.change);
+    });
+    $(".button:contains('Remove')").click(function() {
+        $(".append .added:last-child").remove();
     });
 });
