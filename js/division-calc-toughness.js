d9e785a1d6ce7e46c5d8cfefe7862a9f21556381
Zerthox <zerthox@gmail.com>
2017-06-22 16:19:42 +0200

    Fix update functionality

--- js/division-calc-toughness.js
+++ js/division-calc-toughness.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
 	$("head").append("<script src='/js/division-calc.js'></script>");
-    $("input").on("keyup change", function() {
-    	var s = 15 * parseFloat($(this).parents(".flex-list").find(".stamina").val()) || 0;
+    $("input").on("keyup change", page.update);
+});
+
+// PAGE UPDATE
+var page = {
+    update: function() {
+        var s = 15 * parseFloat($(this).parents(".flex-list").find(".stamina").val()) || 0;
     	var h = parseFloat($(this).parents(".flex-list").find(".health").val()) || 0;
         $(this).parents(".flex-row").find(".output").text(Math.round(calc.toughness({
           	mitigation: calc.mitigation($(this).parents(".flex-list").find(".armor").val()),
           	health: h + s
         })));
-    });
-});
+    }
+};
