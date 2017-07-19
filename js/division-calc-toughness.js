893a1a4cda9de7ba894eb622d98dabf1333c89b1
Zerthox <zerthox@gmail.com>
2017-07-19 20:35:28 +0200

    Formatting

--- js/division-calc-toughness.js
+++ js/division-calc-toughness.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 // PAGE UPDATE
 var page = {
     update: function() {
-        var s = 15 * parseFloat($(this).parents(".flex-list").find(".stamina").val()) || 0;
-    	var h = parseFloat($(this).parents(".flex-list").find(".health").val()) || 0;
+      var s = 15 * parseFloat($(this).parents(".flex-list").find(".stamina").val()) || 0,
+    	h = parseFloat($(this).parents(".flex-list").find(".health").val()) || 0;
         $(this).parents(".flex-row").find(".output").text(Math.round(calc.toughness({
           	mitigation: calc.mitigation($(this).parents(".flex-list").find(".armor").val()),
           	health: h + s
